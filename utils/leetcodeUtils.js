2de83be2e1381c54a82cbbaec1632bd511fd9df1
Ashu <abhishekaggarwal2801@gmail.com>
2024-06-25 23:22:59 +0530

    Leetcode user object incorporated error management

--- utils/leetcodeUtils.js
+++ utils/leetcodeUtils.js
@@ -1,17 +1,17 @@
 function leetcodeDataHandler(updatedUser) {
   const leetcode = {
-    username: updatedUser.matchedUser.username,
+    username: updatedUser?.matchedUser?.username ?? '',
     problems_solved: {
-      all: updatedUser.matchedUser.submitStats.acSubmissionNum[0].count,
-      easy: updatedUser.matchedUser.submitStats.acSubmissionNum[1].count,
-      medium: updatedUser.matchedUser.submitStats.acSubmissionNum[2].count,
-      hard: updatedUser.matchedUser.submitStats.acSubmissionNum[3].count,
+      all: updatedUser?.matchedUser?.submitStats?.acSubmissionNum?.[0]?.count ?? 0,
+      easy: updatedUser?.matchedUser?.submitStats?.acSubmissionNum?.[1]?.count ?? 0,
+      medium: updatedUser?.matchedUser?.submitStats?.acSubmissionNum?.[2]?.count ?? 0,
+      hard: updatedUser?.matchedUser?.submitStats?.acSubmissionNum?.[3]?.count ?? 0,
     },
-    global_ranking: updatedUser.userContestRanking.globalRanking,
-    top_percentage: updatedUser.userContestRanking.topPercentage,
-    badge: updatedUser.userContestRanking.badge.name,
+    global_ranking: updatedUser?.userContestRanking?.globalRanking ?? 0,
+    top_percentage: updatedUser?.userContestRanking?.topPercentage ?? 0,
+    badge: updatedUser?.userContestRanking?.badge?.name ?? 'No Badge',
   };
   return leetcode;
 }
 
-module.exports = {leetcodeDataHandler};
+module.exports = { leetcodeDataHandler };
