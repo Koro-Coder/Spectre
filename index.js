38669439701d8bc0e005c23edbdcd306c3854745
Ashu <abhishekaggarwal2801@gmail.com>
2024-06-26 13:48:55 +0530

    deleted unnecessary files

--- index.js
+++ index.js
@@ -4,82 +4,61 @@ const { MongoStore } = require('wwebjs-mongo');
 const mongoose = require('mongoose');
 const {handleCommands} = require('./controllers/handleCommands.js')
 const {handleLatestUpdates} = require('./controllers/handlelLatestUpdates.js');
-const {run} = require('./controllers/commands/groupInfo.js');
 const { checkGroup } = require('./controllers/checks/checkGroup.js');
 const { sendToOneGroup } = require('./controllers/messageDistribution/groupRouting.js');
 const { addNewGroup } = require('./controllers/updateGroupParticipants.js');
+const { onGroupJoin, addedMembers, activatedBot } = require('./utils/replies.js');
+const schedule = require('node-schedule');
+const { postDailyChallenge } = require('./controllers/postDailyChallenge.js');
 
-require('dotenv').config()
+require('dotenv').config();
 
-mongoose.connect(process.env.DB_STRING).then(async() => {
-    // const msg = await handleCommands("919650236159", "groupid", "mentions[0]", "/add lc profile - mrgamer2801 ");
-    // console.log(msg);
-    // while(true)
-    // {
-    //     await handleLatestUpdates();
-    // }
+///////////////////////////////
+
+const client = new Client({
+    webVersionCache: {
+        type: "remote",
+        remotePath:
+          "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    }});
+
+client.on('ready', async() => {
+    console.log('Client is ready!');
+    schedule.scheduleJob('0 9 * * *', async () => {
+        await postDailyChallenge(client);
+    });
+    while(true)
+    {
+        await handleLatestUpdates(client);
+    }
 });
 
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
 
-/*
+client.initialize();
+
 mongoose.connect(process.env.DB_STRING).then(async() => {
-    console.log("Connected to server");
-    const store = new MongoStore({ mongoose: mongoose });
-    const client = new Client({
-        webVersionCache: {
-            type: "remote",
-            remotePath:
-              "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-          },
-        authStrategy: new RemoteAuth({
-            store: store,
-            backupSyncIntervalMs: 300000
-        })
-    });
-    client.once('qr', (qr) => {
-        qrcode.generate(qr, {small: true});
-    });
-    
-    client.once('ready', () => {
-        console.log('Client is ready!');
-    });
-
-    client.on('remote_session_saved', () => {
-        console.log('Session Saved');
-    });
-
-    client.on('group_join', async(notification)=>{
-        console.log('Group Joined :: ',notification);
-        const participantId = notification.recipientIds[0];
-        const participant = await client.getContactById(participantId);
-        console.log(participant);
-    })
-    
     client.on('message', async (msg) => {
-    
-        //const c1 = await msg.getContact();
-        //console.log('getContact - ', c1);
-    
-        //const c2 = msg.from;
-        //console.log('from  - ', c2);
-    
-        // if (msg.body == '!ping') {
-        //     client.sendMessage(msg.from,'pong');
-        // }
-
+        console.log(msg.body);
         if(msg.body[0] == '/' && checkGroup(msg.from))
         {
-            const reply = await handleCommands();
+            const reply = await handleCommands(msg.author.slice(0, -5), msg.from, msg.mentionedIds, msg.body);
             sendToOneGroup(msg, reply);
         }
-
-        if(msg.body.replace(/\s+/g, "").toLowerCase() == '-activatespectrebot')
+        else if(msg.body.replace(/\s+/g, "").toLowerCase() == '-activatespectrebot')
         {
             await addNewGroup(msg, client);
-            client.sendMessage(msg.from,'🤖 Spectre Bot is active now.');
+            sendToOneGroup(msg, activatedBot);
+        }
+        else if(msg.body.replace(/\s+/g, "").toLowerCase() == '-addnewmembers' && checkGroup(msg.from))
+        {
+            await addNewGroup(msg, client);
+            sendToOneGroup(msg, addedMembers);
         }
     });
-
-    client.initialize();
+    client.on('group_join', async(notification)=>{
+        client.sendMessage(notification.chatId, onGroupJoin);
+    })
 });
-*/
