eb3167c6572e96f2fbd0d4b9f27b5e206bc85118
Ashu <abhishekaggarwal2801@gmail.com>
2024-06-27 19:37:36 +0530

    Url send through mail

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client, RemoteAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode');
+const schedule = require('node-schedule');
+const nodemailer = require('nodemailer');
 const { MongoStore } = require('wwebjs-mongo');
 const mongoose = require('mongoose');
 const {handleCommands} = require('./controllers/handleCommands.js')
@@ -8,12 +10,16 @@ const { checkGroup } = require('./controllers/checks/checkGroup.js');
 const { sendToOneGroup } = require('./controllers/messageDistribution/groupRouting.js');
 const { addNewGroup } = require('./controllers/updateGroupParticipants.js');
 const { onGroupJoin, addedMembers, activatedBot } = require('./utils/replies.js');
-const schedule = require('node-schedule');
 const { postDailyChallenge } = require('./controllers/postDailyChallenge.js');
-
 require('dotenv').config();
 
-///////////////////////////////
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD 
+    }
+});
 
 const client = new Client({
     webVersionCache: {
@@ -22,19 +28,40 @@ const client = new Client({
           "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
     }});
 
-client.on('ready', async() => {
+client.once('ready', async() => {
     console.log('Client is ready!');
     schedule.scheduleJob('0 9 * * *', async () => {
         await postDailyChallenge(client);
     });
-    while(true)
-    {
+    while(true){
         await handleLatestUpdates(client);
     }
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.once('qr', qr => {
+    qrcode.toDataURL(qr, (err, url) => {
+        if (err) {
+            console.error('Error generating QR code:', err);
+            return;
+        }
+        const mailOptions = {
+            from: {
+                name: 'Spectre',
+                address: process.env.EMAIL
+            },
+            to: process.env.EMAIL,
+            subject: 'WhatsApp QR Code',
+            text: 'Copy this URL in browser and scan QR code to log into Whatsapp Web \n\n' + url
+
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+            } else {
+                console.log('Email sent:', info.response);
+            }
+        });
+    });
 });
 
 client.initialize();
